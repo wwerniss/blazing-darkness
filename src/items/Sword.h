8c6e657acc9c1786f2a9273a2e630e973ad30962
Daria Kozyrska <dora26kozir@gmail.com>
2025-11-22 03:55:55 +0200

    Doxygen documentation generation

--- src/items/Sword.h
+++ src/items/Sword.h
@@ -1,9 +1,38 @@
 #pragma once
 
+/**
+ * @brief A basic sword weapon.
+ * 
+ * This is a standard sword that provides moderate damage and is more powerful than the stick.
+ * It serves as an intermediate weapon in the game progression.
+ * 
+ * @example
+ * Sword sword;
+ * player->equipWeapon(&sword);
+ */
 #include "Weapon.h"
 
+/**
+ * @brief A basic sword weapon.
+ * 
+ * This is a standard sword that provides moderate damage and is more powerful than the stick.
+ * It serves as an intermediate weapon in the game progression.
+ */
 class Sword : public Weapon {
 public:
+    /**
+     * @brief Constructs a new Sword instance.
+     * 
+     * Initializes the sword with default properties like name, description, damage, and value.
+     */
     Sword();
+    
+    /**
+     * @brief Gets the type of item.
+     * 
+     * Returns "sword" as the type for all sword instances.
+     * 
+     * @return "sword" as a string
+     */
     std::string getType() const override;
 };
